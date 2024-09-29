ae94a847d1ad209a2d6884d44589d7046cefa40a
Elhadedy36 <elhadede36@gmail.com>
2024-09-29 08:56:40 +0300

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(Dalel());
+}
+
+
+class Dalel extends StatelessWidget {
+  const Dalel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
