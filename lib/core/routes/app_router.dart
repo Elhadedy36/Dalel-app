0db0b48f9939236f60ab0caf9159e4031c8dd90e
Elhadedy36 <elhadede36@gmail.com>
2024-09-30 12:19:10 +0300

    on boarding and splash

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -4,5 +4,6 @@ import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(routes: [
   GoRoute(path: "/", builder: (context, state) => const Splashview()),
+  
   GoRoute(path: "/onBoarding", builder: (context, state) => const OnBoardingView()),
 ]);
--- lib/features/on_boarding/presentation/widgets/on_boardin_body.dart
+++ lib/features/on_boarding/presentation/widgets/on_boardin_body.dart
@@ -1,9 +1,8 @@
 import 'package:dalel/core/utils/app_assets.dart';
-import 'package:dalel/core/utils/app_colors.dart';
 import 'package:dalel/core/utils/app_text_style.dart';
+import 'package:dalel/features/on_boarding/data/models/on_boarding_model.dart';
 import 'package:dalel/features/on_boarding/presentation/widgets/custom_smooth_page_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class onBoardingBody extends StatelessWidget {
   onBoardingBody({super.key});
@@ -18,7 +17,7 @@ class onBoardingBody extends StatelessWidget {
         
 
         controller: _controller,
-        itemCount: 3,
+        itemCount: onBoardingData.length,
         itemBuilder: (context, index) {
           return Column(
             children: [
@@ -27,7 +26,7 @@ class onBoardingBody extends StatelessWidget {
                 height: 290,
                 width: 343,
                 decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(Assets.imagesOnBoarding1),
+                  image: DecorationImage(image: AssetImage(onBoardingData[index].imagePath),
                   fit: BoxFit.fill
                   )
                 ),
@@ -42,13 +41,13 @@ class onBoardingBody extends StatelessWidget {
             Text(
               maxLines: 2,
               overflow: TextOverflow.ellipsis,
-              'Explore The history with Dalel in a smart way',style: CustomTextStyles.poppins400style24.copyWith(fontWeight: FontWeight.bold),textAlign: TextAlign.center,)
+              onBoardingData[index].title,style: CustomTextStyles.poppins400style24.copyWith(fontWeight: FontWeight.bold),textAlign: TextAlign.center,)
              ,
               SizedBox(
                 height: 16,
               ),
             Text(maxLines: 2,
-              overflow: TextOverflow.ellipsis,'Using our app’s history libraries you can find many historical periods ',style: CustomTextStyles.poppins300style16,textAlign: TextAlign.center,)
+              overflow: TextOverflow.ellipsis,onBoardingData[index].subtitle,style: CustomTextStyles.poppins300style16,textAlign: TextAlign.center,)
             ],
           );
         },
